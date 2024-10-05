35330845bb5143b1291570e611faa50d5d326a68
Seu Nome <seu-email@exemplo.com>
2024-10-05 12:22:28 -0300

    wip(ShippifyProject): structure project setted

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { server } from './server/Server';
 
-server.listen(3333, () => console.log('Running...'));
+server.listen(process.env.PORT||3333, () => {
+  console.log(`Server running on port: ${process.env.PORT||3333}`);
+});
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,9 +1,16 @@
 import  { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
 
 router.get('/', (_, res: any) => {
     return res.send('first commit, all set')
+})
+
+router.post('/drivers', (req, res: any) => {
+    console.log(req.body);
+    
+    return res.status(StatusCodes.ACCEPTED).send(req.body)
 })
 
 
